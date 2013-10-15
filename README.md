2faf36572985621748084a678f1abf2497fe91f3
Anton Lindström <lindztr@gmail.com>
2013-10-15 17:55:28 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Linux API Tests
+
+Simple C programs against the Linux API.
+
+This is a project to learn the Linux API better as well as learn some C.
---
+++ glibc_version/glibc_version.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gnu/libc-version.h>
+
+#ifdef __GLIBC__
+#include <unistd.h>
+#endif
+
+const char *gnu_get_libc_version(void);
+
+int main() {
+  char *glibc_version;
+  size_t len;
+
+  len = confstr(_CS_GNU_LIBC_VERSION, NULL, (size_t)0);
+  glibc_version = malloc(len);
+
+  confstr(_CS_GNU_LIBC_VERSION, glibc_version, len);
+
+  printf("GNU C Library version, gnu_get_libc_version(): %s\n", gnu_get_libc_version());
+  printf("GNU C Library version, _CS_GNU_LIBC_VERSION(): %s\n", glibc_version);
+
+  return 0;
+}
