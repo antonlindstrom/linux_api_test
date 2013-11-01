b1130df986922549eededa4230e5704a3f4d7f82
Anton Lindström <lindztr@gmail.com>
2013-11-01 11:52:45 +0000

    fix append flag for tee-example

--- io/tee-example.c
+++ io/tee-example.c
@@ -2,13 +2,23 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <getopt.h>
 
 #define READ_BUFFER 8192
 
 int main (int argc, char *argv[]) {
-  int fd;
+  int fd, opt, flags;
   char buffer[READ_BUFFER];
   ssize_t numRead, numWrite;
+
+  flags = O_RDWR | O_TRUNC | O_CREAT;
+
+  // Set flags to append if opt -a
+  while((opt = getopt(argc, argv, "a:")) != -1) {
+    if (opt = 'a') {
+      flags = O_RDWR | O_APPEND | O_CREAT;
+    }
+  }
 
   numRead = read(STDIN_FILENO, buffer, READ_BUFFER);
 
@@ -19,22 +29,25 @@ int main (int argc, char *argv[]) {
 
   buffer[numRead] = '\0';
 
+  // Write to STDOUT and exit
   if (argc == 1) {
     printf("%s", buffer);
-  } else {
-    fd = open(argv[1], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    exit(EXIT_SUCCESS);
+  }
 
-    if (fd == -1) {
-      printf("Error: open\n");
-      exit(EXIT_FAILURE);
-    }
+  // Write to file specified last of args
+  fd = open(argv[argc-1], flags, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
 
-    numWrite = write(fd, buffer, numRead);
+  if (fd == -1) {
+    printf("Error: failed to open %s\n", argv[argc-1]);
+    exit(EXIT_FAILURE);
+  }
 
-    if (numWrite == -1) {
-      printf("Error: write\n");
-      exit(EXIT_FAILURE);
-    }
+  numWrite = write(fd, buffer, numRead);
+
+  if (numWrite == -1) {
+    printf("Error: failed to write %s\n", argv[argc-1]);
+    exit(EXIT_FAILURE);
   }
 
   exit(EXIT_SUCCESS);
