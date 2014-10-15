aded8084c783e66a56959e924cde195409280107
Anton Lindström <lindztr@gmail.com>
2014-10-15 18:01:38 +0000

    remove unnecessary headers in inode_stat

--- io/inode_stat.c
+++ io/inode_stat.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <time.h>
 
 int main(int argc, char *argv[]) {
   struct stat node;
